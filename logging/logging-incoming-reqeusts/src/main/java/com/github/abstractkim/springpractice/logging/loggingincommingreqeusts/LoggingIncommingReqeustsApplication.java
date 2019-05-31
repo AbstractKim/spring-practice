7a028489d2636758536bec7b6731c480863855e5
kimbe7w <szone79TALA79!>
2019-05-31 11:45:52 -0400

    create 'logging-incomming-reqeusts' as spring-boot web

---
+++ logging/logging-incoming-reqeusts/src/main/java/com/github/abstractkim/springpractice/logging/loggingincommingreqeusts/LoggingIncommingReqeustsApplication.java
@@ -0,0 +1,13 @@
+package com.github.abstractkim.springpractice.logging.loggingincommingreqeusts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoggingIncommingReqeustsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoggingIncommingReqeustsApplication.class, args);
+    }
+
+}
---
+++ logging/logging-incoming-reqeusts/src/test/java/com/github/abstractkim/springpractice/logging/loggingincommingreqeusts/LoggingIncommingReqeustsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.abstractkim.springpractice.logging.loggingincommingreqeusts;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LoggingIncommingReqeustsApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
